7d352e7c08faec3bef77226729142331d4934a98
Akinpennu Taiwo <moriyebas@gmail.com>
2025-04-10 20:42:28 +0300

    potatodisease1

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://potatoes-leaf-disease-detection.onrender.com/predict","http://localhost:5173"],  # Allow all origins
+    allow_origins=["https://potatoes-leaf-disease-detection.onrender.com/predict","http://localhost:5173", "*"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
